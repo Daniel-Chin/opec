4623f2617db21680c88d8ba0843eb602cb32c7b4
Daniel Chin <daniel_chin@yahoo.com>
2020-03-08 23:56:40 -0400

    Grredy

--- auction_assistant/script.js
+++ auction_assistant/script.js
@@ -1,4 +1,8 @@
 const NATIONS = ['Saudi', 'Iran', 'Venezuela', 'Iraq'];
+var scenario = null;
+var greed = null;
+var recommend = .4;
+const drift = .427;
 
 const generateTable = () => {
   const table = document.getElementById('table');
@@ -22,13 +26,33 @@ const generateTable = () => {
       const cell = newCell();
       row.appendChild(cell);
       cell.id = `cell-${x}-${nation}`;
+      if (! isNonStrategic(x, nation)) {
+        cell.classList.add('hoverGray');
+        cell.onclick = onClick.bind(null, cell, x, nation);
+      }
     });
   });
 };
 
 const onSlider = ({ target }) => {
   document.getElementById('valueDisplay').innerHTML = target.value;
-  updateValuation(target.value / 100);
+  scenario = target.value / 100;
+  updateValuation();
+};
+
+const onGreeder = ({ target }) => {
+  greed = target.value / 100;
+  updateGreedDisplay();
+  updateValuation();
+};
+
+const formatPercent = (value) => (
+  `${Math.round(value * 100)}%`
+);
+
+const updateGreedDisplay = () => {
+  const d = document.getElementById('greedDisplay');
+  d.innerHTML = `${formatPercent(greed)} (Recommend ${formatPercent(recommend)})`;
 };
 
 window.onload = () => {
@@ -36,6 +60,9 @@ window.onload = () => {
   const slider = document.getElementById("slider");
   slider.oninput = onSlider;
   onSlider({ target: slider });
+  const greeder = document.getElementById("greeder");
+  greeder.oninput = onGreeder;
+  onGreeder({ target: greeder });
 };
 
 const DATA = {
@@ -44,26 +71,51 @@ const DATA = {
   BBC: [ 3748, 502, 367, null ], 
 };
 
-const updateValuation = (ratio) => {
+const updateValuation = () => {
   const twoSides = {
     A: ['A', 'B'], 
     B: ['A', 'BBC'], 
     C: ['A', 'BBC'], 
   };
-  const mask = [1 - ratio, ratio];
+  const mask = [1 - scenario, scenario];
   [...'ABC'].forEach((market) => {
     NATIONS.forEach((nation, j) => {
       const cell = document.getElementById(
         `cell-${market}-${nation}`
       );
-      if (market !== 'A' && nation === 'Iraq') {
+      if (cell.classList.contains('sold')) return;
+      if (isNonStrategic(market, nation)) {
         cell.innerHTML = 'N/A';
         return;
       }
-      const value = twoSides[market].map((scene, i) => (
+      let value = twoSides[market].map((scene, i) => (
         DATA[scene][j] * mask[i]
       )).reduce((x, y) => (x + y));
+      value = value * (1 - greed) + 100 * greed;
       cell.innerHTML = value.toFixed(0);
     });
   });
 };
+
+const onClick = (cell, market, nation) => {
+  const saved_greed = greed;
+  greed = 0;
+  updateValuation();
+  const ref_price = parseInt(cell.innerHTML);
+  greed = saved_greed;
+  updateValuation();
+  const op = prompt(`Market ${market} ${nation} sold at:`, ref_price);
+  if (op === null) return;
+  const price = parseInt(op);
+  cell.innerHTML = price;
+  cell.classList.remove('hoverGray');
+  cell.classList.add('sold');
+  const this_greed = 1 - (price - 100) / (ref_price - 100);
+  recommend = recommend * (1 - drift) + this_greed * drift;
+  recommend = Math.max(0, recommend);
+  updateGreedDisplay();
+};
+
+const isNonStrategic = (market, nation) => (
+  market !== 'A' && nation === 'Iraq'
+);
